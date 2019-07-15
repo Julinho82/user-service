5523ddec8d108c25554a4098f3051f9fa305736a
JULIO <juliolotr@gmail.com>
2019-07-15 01:02:32 -0400

    igualando repositorio docente

--- src/main/java/com/dh/fullstack/users/service/model/domain/Account.java
+++ src/main/java/com/dh/fullstack/users/service/model/domain/Account.java
@@ -1,17 +1,19 @@
 package com.dh.fullstack.users.service.model.domain;
 
-
 import javax.persistence.*;
 
+/**
+ * @author Julio Daviu
+ */
 @Entity
-@Table(name = "account_table", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {
-                "email"
-        })
-})
+@Table(name = "account_table",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})}
+)
 public class Account {
+
+    //CTRL+ALT+L
     @Id
-    @Column(name = "accountid", nullable = false)
+    @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
@@ -28,5 +30,21 @@ public class Account {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public AccountState getState() {
+        return state;
+    }
+
+    public void setState(AccountState state) {
+        this.state = state;
     }
 }
