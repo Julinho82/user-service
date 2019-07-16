b28753c8bbc2f992a1ef4a91e472b3008cbdf5b0
JULIO <juliolotr@gmail.com>
2019-07-16 08:38:41 -0400

    test rename BeanTestController

--- src/main/java/com/dh/fullstack/users/service/controller/BeanTestController.java
+++ src/main/java/com/dh/fullstack/users/service/controller/BeanTestController.java
@@ -1,27 +1,42 @@
 package com.dh.fullstack.users.service.controller;
 
 import com.dh.fullstack.users.service.bean.Asus;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.RequestScope;
 
+
 @RestController
 @RequestMapping ("/accounts") //para colocar el path
 @RequestScope
-public class AccountController {
+public class BeanTestController {
 
+    private Integer value=1;
 
+    @Autowired//Inyeccion por propiedad
     private Asus asus;
 
-    public AccountController(Asus asus) {
-        this.asus = asus;
-    }
+// Inyeccion por constructor
+//    public AccountController(Asus asus) {
+//        this.asus = asus;
+//    }
+
+// Inyeccion por Setter
+//    @Autowired
+//    public void setAsus(Asus asus) {
+//        this.asus = asus;
+//    }
 
     @RequestMapping(value="/asus", method = RequestMethod.GET)
     public Asus readAsus(){
 
-        asus.setName(asus.getName()+":GET");
+        value = value + 1;
+        asus.setName(asus.getName()+":GET"+ ", Value: " +value);
         return asus;
     }
+
+
 }
