4bda7f92cea27bd7da1f62ac9fcdc6a1b3b28f73
JULIO <juliolotr@gmail.com>
2019-07-17 09:21:53 -0400

    documentation of model Account

--- src/main/java/com/dh/fullstack/users/service/model/domain/Account.java
+++ src/main/java/com/dh/fullstack/users/service/model/domain/Account.java
@@ -1,5 +1,8 @@
 package com.dh.fullstack.users.service.model.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 
 /**
@@ -9,19 +12,23 @@ import javax.persistence.*;
 @Table(name = "account_table",
         uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})}
 )
+@ApiModel(description="All details about the account")
 public class Account {
 
     //CTRL+ALT+L
     @Id
     @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @ApiModelProperty(notes = "The account id is generated for the Data Base")
     private Long id;
 
     @Column(name = "email", length = 100, nullable = false)
+    @ApiModelProperty(notes = "The account email")
     private String email;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "state", length = 20, nullable = false)
+    @ApiModelProperty(notes = "The account state")
     private AccountState state;
 
     public Long getId() {
